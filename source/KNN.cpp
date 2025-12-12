fde2e83190ae6db84640c33032f41f50e4a290fe
Dhruv Sonavane <sonavanebharat92@gmail.com>
2025-12-12 14:19:36 +0530

    Update KNN.cpp

--- source/KNN.cpp
+++ source/KNN.cpp
@@ -65,58 +65,34 @@ int Knn(int a,int b,double *values,double *test)
 	vector<double> ans2;
 	int clss[b],ct=0;
 	vector<double> ans3;
+	
 	for (int i = 0; i < a; ++i)
 		for (int j = 0; j < b; ++j) ans[i][j] = ((*(test + i) - *(values + i * b + j)) * (*(test + i) - *(values + i * b + j)));
+	
 	for (int i = 0; i < a; ++i)
-		for (int j = 0; j < b; ++j)
-			ans2.push_back(ans[i][j] + ans[i+1][j]);
-	for (int i = 0; i < ans2.size()/a; ++i)
-	{
-		ans3.push_back(sqrt(ans2[i])); 
-	}
+		for (int j = 0; j < b; ++j) ans2.push_back(ans[i][j] + ans[i+1][j]);
+	for (int i = 0; i < ans2.size()/a; ++i) ans3.push_back(sqrt(ans2[i])); 
 	sort(ans3.begin(),ans3.end());
 	for (int i = 0; i < 3; ++i)
-	{
 		for (int j = 0; j < ans2.size() / a; ++j)
-		{
 			if(ans3[i]==sqrt(ans2[j]))
 			{
 				clss[ct] = cls[j];
 				ct++;
 			}
-		}
-	}
 	cout << "Values " << " \t" << "Class" << endl;
 	cout << "--------------------------------------" << endl;
-	for (int i = 0; i < ans2.size() / a; ++i)
-	{
-		cout << sqrt(ans2[i]) << "\t   \t" << cls[i] << endl;
-	}
+	for (int i = 0; i < ans2.size() / a; ++i) cout << sqrt(ans2[i]) << "\t   \t" << cls[i] << endl;
 	cout << endl << "K = 3" << "\t" << "Class" << endl;
 	cout << "------------------------------" << endl;
-	for (int i = 0; i < 3; ++i)
-	{
-		cout << ans3[i] << "\t  " << clss[i] << endl;
-	}
+	for (int i = 0; i < 3; ++i) cout << ans3[i] << "\t  " << clss[i] << endl;
 	int y=0,n=0;
 	for (int i = 0; i < 3; ++i)
 	{
-		if(clss[i]==1)
-		{
-			++y;
-		}
-		else if (clss[i]==0)
-		{
-			++n;
-		}	
+		if(clss[i]==1) ++y;
+		else if (clss[i]==0) ++n;
 	}
-	if (y>n)
-	{
-		cout << endl << "Yes" << endl;
-	}
-	else
-	{
-		cout << endl << "No" << endl;
-	}		
+	if (y>n) cout << endl << "Yes" << endl;
+	else cout << endl << "No" << endl;	
 	return 0;
 }
